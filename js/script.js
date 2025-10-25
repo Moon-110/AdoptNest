bda7d64a50f54462c6ad9915b77fa14441673e89
Moon110 <moynul1.islam0@gmail.com>
2025-10-25 16:10:56 +0600

    load categories by clicking buttons

--- js/script.js
+++ js/script.js
@@ -35,6 +35,33 @@ style.innerHTML = `
     background-color: green;
     scale: 1.05;
   }
+    /* Pet Cards Grid */
+  #pets-container {
+    display: grid;
+    grid-template-columns: repeat(3, minmax(240px, 1fr));
+    gap: 18px;
+    padding: 20px;
+  }
+
+  .pet-card {
+    background: white;
+    border-radius: 10px;
+    padding: 12px;
+    box-shadow: 0 0 8px rgba(0,0,0,0.08);
+    text-align: left;
+  }
+  .pet-card img {
+    width: 100%;
+    height: 180px;
+    object-fit: cover;
+    border-radius: 8px;
+    margin-bottom: 8px;
+  }
+  .pet-title {
+    font-size: 18px;
+    font-weight: 600;
+    margin-bottom: 6px;
+  }
 `;
 document.head.appendChild(style);
 
@@ -64,11 +91,62 @@ const displayCategories = (categories) => {
         .forEach(btn => btn.classList.remove("active"));
       button.classList.add("active");
 
-      console.log("Selected Category:", category.category);
+      loadPetsByCategory(category.category);
+
+    //   console.log("Selected Category:", category.category);
     });
 
     container.appendChild(button);
   });
 };
 
+// Load pets by category
+const loadPetsByCategory = async (categoryName) => {
+  const url = `https://openapi.programming-hero.com/api/peddy/category/${categoryName}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPets(data.data); // API returns data.data here
+};
+
+// Load All Pets Initially
+const loadPets = async () => {
+  const url = "https://openapi.programming-hero.com/api/peddy/pets";
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPets(data.pets);
+};
+
+
+// Show Pets in UI
+const displayPets = (pets) => {
+  const container = document.getElementById("pets-container");
+  container.innerHTML = "";
+
+  pets.forEach(pet => {
+    const card = document.createElement("div");
+    card.classList.add("pet-card");
+
+    card.innerHTML = `
+      <img src="${pet.image}" alt="${pet.pet_name}">
+      <h3 class="pet-title">${pet.pet_name}</h3>
+
+      <div class="pet-info">
+        <p>Breed: ${pet.breed || "Not Available"}</p>
+        <p>Birth: ${pet.date_of_birth || "Unknown"}</p>
+        <p>Gender: ${pet.gender || "Unknown"}</p>
+        <p>Price: ${pet.price ? pet.price + " ৳" : "Not Provided"}</p>
+      </div>
+
+      <div class="pet-buttons">
+        <button class="pet-btn like-btn">❤</button>
+        <button class="pet-btn adopt-btn">Adopt</button>
+        <button class="pet-btn details-btn">Details</button>
+      </div>
+    `;
+
+    container.appendChild(card);
+  });
+};
+
 loadCategories();
+loadPets();
